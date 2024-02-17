4641435055cf1dab92b22754221f1322b6870b08
Ashutosh <1ashutoshsahu@gmail>
2024-02-17 13:20:30 +0530

    Sign up to firebase

--- app/src/main/java/com/example/shoppingapp/ui_layer/Sheets/Activitys/SignUp/SignUpActivity.kt
+++ app/src/main/java/com/example/shoppingapp/ui_layer/Sheets/Activitys/SignUp/SignUpActivity.kt
@@ -4,24 +4,52 @@ import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.widget.AppCompatButton
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.shoppingapp.Comman.returnText
 import com.example.shoppingapp.R
 import com.example.shoppingapp.databinding.ActivitySignUpBinding
 import com.example.shoppingapp.ui_layer.Sheets.Activitys.Home.HomeActivity
 import com.example.shoppingapp.ui_layer.Sheets.Activitys.Login.LoginActivity
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.Circle
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class SignUpActivity : AppCompatActivity() {
     private val binding: ActivitySignUpBinding by lazy {
         ActivitySignUpBinding.inflate(layoutInflater)
     }
 
-    private lateinit var firstName: String
-    private lateinit var lastName: String
-    private lateinit var email: String
-    private lateinit var password: String
-    private lateinit var confirmPassword: String
+//    private lateinit var firstName: String
+//    private lateinit var lastName: String
+//    private lateinit var email: String
+//    private lateinit var password: String
+//    private lateinit var confirmPassword: String
 
+
+    private val signUpActivityViewModel by viewModels<SignUpActivityViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return SignUpActivityViewModel(this@SignUpActivity) as T
+                }
+            }
+        }
+    )
+
+    //    val viewModel = ViewModelProvider(this)[SignUpActivityViewModel::class.java]
+    fun loader() {
+        // code for loader
+        val progressBar = binding.loader as ProgressBar
+        val circle: Sprite = Circle()
+        progressBar.indeterminateDrawable = circle
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,37 +58,53 @@ class SignUpActivity : AppCompatActivity() {
         binding.apply {
 
             signUpBtn.setOnClickListener {
-
-                firstName = inputFirstName.text.toString().trim()
-                lastName = inputLastName.text.toString().trim()
-                email = inputEmail.text.toString().trim()
-                password = inputPassword.text.toString().trim()
-                confirmPassword = inputConfirmPassword.text.toString().trim()
+                loader()
+//                firstName = inputFirstName.text.toString().trim()
+//                lastName = inputLastName.text.toString().trim()
+//                email = inputEmail.text.toString().trim()
+//                password = inputPassword.text.toString().trim()
+//                confirmPassword = inputConfirmPassword.text.toString().trim()
 
                 inputFirstName.setBackgroundResource(R.drawable.edit_text_shape)
                 inputEmail.setBackgroundResource(R.drawable.edit_text_shape)
                 inputPassword.setBackgroundResource(R.drawable.edit_text_shape)
                 inputConfirmPassword.setBackgroundResource(R.drawable.edit_text_shape)
 
-                if (firstName.isBlank() || email.isBlank() || password.isBlank() || confirmPassword.isBlank()) {
-                    if (firstName.isBlank()) {
-                        inputFirstName.setBackgroundResource(R.drawable.edit_text_shape_error)
-                    }
-                    if (email.isBlank()) {
+                if (returnText(inputEmail).isBlank() || returnText(inputPassword).isBlank() || returnText(
+                        inputConfirmPassword
+                    ).isBlank()
+                ) {
+//                    if (firstName.isBlank()) {
+//                        inputFirstName.setBackgroundResource(R.drawable.edit_text_shape_error)
+//                    }
+                    if (returnText(inputEmail).isBlank()) {
                         inputEmail.setBackgroundResource(R.drawable.edit_text_shape_error)
                     }
-                    if (password.isBlank()) {
+                    if (returnText(inputPassword).isBlank()) {
                         inputPassword.setBackgroundResource(R.drawable.edit_text_shape_error)
                     }
-                    if (confirmPassword.isBlank()) {
-
+                    if (returnText(inputConfirmPassword).isBlank()) {
                         inputConfirmPassword.setBackgroundResource(R.drawable.edit_text_shape_error)
                     }
                     Toast.makeText(
                         this@SignUpActivity, "Please fill all the details!", Toast.LENGTH_SHORT
                     ).show()
                 } else {
+                    if (inputPassword.text.toString() == inputConfirmPassword.text.toString()) {
+                        loader.visibility = View.VISIBLE
+                        signUpActivityViewModel.signUp(
+                            returnText(inputEmail),
+                            returnText(inputConfirmPassword)
+                        )
+                        loader.visibility = View.GONE
 
+                    } else {
+                        inputPassword.setBackgroundResource(R.drawable.edit_text_shape_error)
+                        inputConfirmPassword.setBackgroundResource(R.drawable.edit_text_shape_error)
+                        Toast.makeText(
+                            this@SignUpActivity, "Password don't not Match !", Toast.LENGTH_SHORT
+                        ).show()
+                    }
 
                 }
             }
--- app/src/main/java/com/example/shoppingapp/ui_layer/Sheets/Activitys/SignUp/SignUpActivityViewModel.kt
+++ app/src/main/java/com/example/shoppingapp/ui_layer/Sheets/Activitys/SignUp/SignUpActivityViewModel.kt
@@ -1,43 +1,59 @@
 package com.example.shoppingapp.ui_layer.Sheets.Activitys.SignUp
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
-import android.os.Build
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
-import android.view.Window
-import android.view.WindowManager
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.ViewModel
 import com.example.shoppingapp.R
 import com.example.shoppingapp.ui_layer.Sheets.Activitys.Home.HomeActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
-class SignUpActivityViewModel(var activity: Activity) : ViewModel() {
+//@SuppressLint("StaticFieldLeak")
+class SignUpActivityViewModel(private var activity: Activity) : ViewModel() {
 
+    fun signUp(email: String, password: String) {
+        Firebase.auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val loader = activity.findViewById<ProgressBar>(R.id.loader)
+                    loader.visibility = View.GONE
+                    showDialog()
+//                    Toast.makeText(
+//                        activity, "Sign up successfully !", Toast.LENGTH_SHORT
+//                    ).show()
+                } else {
+                    Toast.makeText(
+                        activity, it.exception?.localizedMessage, Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
 
-    fun showDialog() {
+    private fun showDialog() {
         // Create a dialog
         val dialog = Dialog(activity)
 //        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
         // Set the content view of the dialog
         dialog.setContentView(R.layout.success_register_dailog)
         dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        /*
+                val lp = WindowManager.LayoutParams()
+                lp.width = WindowManager.LayoutParams.WRAP_CONTENT
+                lp.height = WindowManager.LayoutParams.WRAP_CONTENT
+                if (Build.VERSION_CODES.S <= Build.VERSION.SDK_INT) {
+                    lp.blurBehindRadius = 16
+                }
+                lp.flags = WindowManager.LayoutParams.FLAG_BLUR_BEHIND
 
-        val lp = WindowManager.LayoutParams()
-        lp.width = WindowManager.LayoutParams.WRAP_CONTENT
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT
-        if (Build.VERSION_CODES.S <= Build.VERSION.SDK_INT) {
-            lp.blurBehindRadius = 16
-        }
-        lp.flags = WindowManager.LayoutParams.FLAG_BLUR_BEHIND
-
-        dialog.window?.attributes=lp
-        dialog.setCancelable(false)
-        // Show the dialog
-        dialog.show()
-
+                dialog.window?.attributes = lp
+                dialog.setCancelable(false)
+        */
 
 
         val btn = dialog.findViewById<AppCompatButton>(R.id.doneBtn)
@@ -53,6 +69,9 @@ class SignUpActivityViewModel(var activity: Activity) : ViewModel() {
             Toast.makeText(
                 activity, "Sign up & Login Successfully!", Toast.LENGTH_SHORT
             ).show()
+
         }
+        // Show the dialog
+        dialog.show()
     }
 }
