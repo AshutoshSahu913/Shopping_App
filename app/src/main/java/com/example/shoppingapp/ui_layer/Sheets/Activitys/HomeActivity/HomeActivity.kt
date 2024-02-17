437c499cfdfa9d8e40187f0e72dd5f64d3c04343
Ashutosh <1ashutoshsahu@gmail>
2024-02-17 22:52:50 +0530

    change in bottom navigation

--- app/src/main/java/com/example/shoppingapp/ui_layer/Sheets/Activitys/HomeActivity/HomeActivity.kt
+++ app/src/main/java/com/example/shoppingapp/ui_layer/Sheets/Activitys/HomeActivity/HomeActivity.kt
@@ -1,8 +1,9 @@
-package com.example.shoppingapp.ui_layer.Sheets.Activitys.Home
+package com.example.shoppingapp.ui_layer.Sheets.Activitys.HomeActivity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation.findNavController
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.shoppingapp.R
@@ -18,7 +19,11 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBar=insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBar.left, systemBar.top, systemBar.right, -1)
+            insets
+        }
         val navController = findNavController(R.id.container)
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         setupWithNavController(bottomNav, navController)
