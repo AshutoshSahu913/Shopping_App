5b715ca2d116b9469851b6db41298f2b5d5d4fa9
Ashutosh <1ashutoshsahu@gmail>
2024-02-17 13:23:26 +0530

    check user already login or not

---
+++ app/src/main/java/com/example/shoppingapp/ui_layer/Sheets/Activitys/SplashScreen/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.example.shoppingapp.ui_layer.Sheets.Activitys.SplashScreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.shoppingapp.R
+import com.example.shoppingapp.databinding.ActivitySplashScreenBinding
+import com.example.shoppingapp.ui_layer.Sheets.Activitys.Home.HomeActivity
+import com.example.shoppingapp.ui_layer.Sheets.Activitys.Login.LoginActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+class SplashScreen : AppCompatActivity() {
+    val binding: ActivitySplashScreenBinding by lazy {
+        ActivitySplashScreenBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (Firebase.auth.currentUser == null) {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+            }
+        }, 500)
+    }
+}
